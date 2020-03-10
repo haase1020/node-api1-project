f84767793540b35d5fd7fb4618c8b074c5c1ae43
Mandi Haase <haase1020@gmail.com>
2020-03-10 07:52:52 -0400

    update index file. Noedemon app crashing

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const express = require('express');
 const shortid = require('shortid');
 const server = express();
 
+let users = [];
+
+server.use(express.json());
+
+server.get('/hello', (req, res) => {
+    res.status(200).json({ hello: "Web 27" })
+});
+
+const PORT = 5000;
+server.listen(PORT, () =>
+console.log(`\n  ** API on http://localhost:${PORT} **\n`)
+);
+//not working
